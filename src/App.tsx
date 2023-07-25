91cc374bcbade8ab74e290cba9a09908d5a90fa0
MadWolfDev <a_r_p@outlook.fr>
2023-07-25 19:22:09 +0200

    Add Tuffy font

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css'
 import { BreathAnimation } from './modules/breath-activity/BreathAnimation';
+import { theme } from './theme/Theme';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from '@mui/material';
 
 export const App = () => {
 
   return (
-    <div className="App">
-      <BreathAnimation />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <BreathAnimation />
+      </div>
+    </ThemeProvider>
   );
 }
 
