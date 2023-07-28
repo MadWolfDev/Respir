dc2596689877fc30e97ede8e86cf113d381d4a68
MadWolfDev <a_r_p@outlook.fr>
2023-07-28 19:39:43 +0200

    ModesButton created

--- src/modules/user-settings/WelcomeScreen.tsx
+++ src/modules/user-settings/WelcomeScreen.tsx
@@ -1,12 +1,23 @@
 import styled from "@emotion/styled";
 import { Button } from "@mui/material";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { RoutePath } from "../../router/RoutePath.type";
 
 export const WelcomeScreen = () => {
     const navigate = useNavigate();
+    const location = useLocation();
     
     return(
+                <ModesButton 
+                    variant='contained'
+                    onClick={() => { 
+                        !location.pathname.includes(RoutePath.defaultBreathModesScreen) ?
+                            navigate(RoutePath.defaultBreathModesScreen) :
+                            navigate(RoutePath.welcomeScreen);
+                    }}
+                >
+                    Cohérence cardiaque
+                </ModesButton>
         <StartButton 
             variant='contained'
             onClick={() => { navigate(RoutePath.breathAnimationScreen);}}
@@ -20,3 +31,8 @@ export const WelcomeScreen = () => {
 const StartButton = styled(Button)`
 align-self : center
 `;
+const ModesButton = styled(Button)`
+    margin-bottom: 1em;
+    align-self: center;
+    width: 15em;
+`
