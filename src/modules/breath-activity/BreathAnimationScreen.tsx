4285a32bc47911a4f05f639372e8d3a2ba6da2a5
MadWolfDev <a_r_p@outlook.fr>
2023-07-28 19:32:08 +0200

    Return button created inside BreathAnimationScreen

--- src/modules/breath-activity/BreathAnimationScreen.tsx
+++ src/modules/breath-activity/BreathAnimationScreen.tsx
@@ -1,12 +1,22 @@
 import styled from "@emotion/styled";
 import { BreathAnimation } from "./BreathAnimation";
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { RoutePath } from "../../router/RoutePath.type";
 
 export const BreathAnimationScreen = ({showAnim}:{showAnim:boolean;}) => {
+    const navigate = useNavigate();
 
     return showAnim ? (
         <AnimContent>
             <BreathAnimation /> 
         </AnimContent>
+            <ReturnButton 
+                variant='contained'
+                onClick={() => { navigate(RoutePath.welcomeScreen); }}
+            >
+                Retour
+            </ReturnButton>
     ) : null;
 };
 
@@ -15,3 +25,9 @@ const AnimContent = styled.div`
   display: flex;
   flex: 1;
 `;
+
+const ReturnButton = styled(Button)`
+    margin-bottom: 1.5em;
+    align-self: center;
+    width:10em;
+`
