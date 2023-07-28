3b791428681d873500c53c6d6ca41a21bd4c373e
MadWolfDev <a_r_p@outlook.fr>
2023-07-28 17:20:56 +0200

    Refacto remove content and add router to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
-import { useState } from 'react';
-
-import { theme } from './theme/Theme';
+import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
-import { Button, CssBaseline } from '@mui/material';
-import styled from '@emotion/styled';
-import { BreathAnimationManager } from './modules/breath-activity/BreathAnimationManager';
+import { theme } from './theme/Theme';
+import { CssBaseline } from "@mui/material";
+import { AppRouter } from "./router/AppRouter";
 
 export const App = () => {
-  const [showAnim, setShowAnim] = useState<boolean>(false);
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppContent>
-        <BreathAnimationManager showAnim={showAnim} />
-        <Footer>
-          <Button 
-            variant="contained"
-            onClick={() => { setShowAnim(!showAnim) }}
-          >
-            On/Off
-          </Button>
-        </Footer>
-      </AppContent>
+      <RouterProvider router = {AppRouter} />
     </ThemeProvider>
   );
-}
-
-const AppContent = styled.div`
-  background: linear-gradient(180deg, #FF8E8E 0%, #FFF7DC 100%);
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: end;
-`
-
-const Footer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 10vh;
-`
-
+};
