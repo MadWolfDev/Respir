b45f9100927e74734f57a7b70979ebe62bb98b6a
MadWolfDev <a_r_p@outlook.fr>
2023-08-02 17:27:37 +0200

    Add router to test because of useNavigate

--- src/modules/user-settings/DefaultBreathModesScreen.test.tsx
+++ src/modules/user-settings/DefaultBreathModesScreen.test.tsx
@@ -1,9 +1,16 @@
 import { render } from '@testing-library/react';
 import { DefaultBreathModesScreen } from './DefaultBreathModesScreen';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('DefaultBreathModesScreen', () => {
   it('should display all the buttons', () => {
-    const component = render(<DefaultBreathModesScreen />);
+    const component = render(
+      <>
+        <BrowserRouter>
+          <DefaultBreathModesScreen />
+        </BrowserRouter>
+      </>
+    );
     expect(component.baseElement).toMatchSnapshot();
   });
 });
