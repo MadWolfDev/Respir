7ff816c3ca82b0d4aa5e643786952872e4bc2840
MadWolfDev <a_r_p@outlook.fr>
2023-08-03 16:21:42 +0200

    BreathAnimation components created and used inside breathAnimationManager

---
+++ src/modules/breath-activity/breath-animations/BreathInAnimation.tsx
@@ -0,0 +1,29 @@
+import { Typography } from '@mui/material';
+import { motion } from 'framer-motion';
+import { useBreathModeStore } from '../../../store/breathModeStore';
+
+export const BreathInAnimation = () => {
+  const animDuration: number = useBreathModeStore(
+    (state) => state.breathModeSelected.breathInDuration
+  );
+
+  const breathInAnimation = {
+    initial: { y: '45vh' },
+    animate: {
+      y: '-40vh',
+      transition: {
+        duration: animDuration,
+      },
+    },
+  };
+
+  return animDuration ? (
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={breathInAnimation}
+    >
+      <Typography variant="h1">Inspirez</Typography>
+    </motion.div>
+  ) : null;
+};
