4ae717a3018c9eca0a54cb30b0521b494dba55fd
MadWolfDev <a_r_p@outlook.fr>
2023-08-07 18:03:59 +0200

    Show the test how to describe properly

--- src/modules/breath-activity/BreathAnimationManager.test.tsx
+++ src/modules/breath-activity/BreathAnimationManager.test.tsx
@@ -1,7 +1,7 @@
 import { act, render, screen } from '@testing-library/react';
 import { BreathAnimationManager } from './BreathAnimationManager';
 
-describe('BreathAnimation', () => {
+describe('BreathAnimationManager', () => {
   beforeEach(() => {
     jest.useFakeTimers();
   });
